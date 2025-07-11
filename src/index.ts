d67ff41521cc5857711777a613d751181b7597db
sv709 <sv709@yahoo.com>
2025-07-11 12:47:59 -0700

    initial commit of server

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPlugin } from '@apollo/server';
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from 'fs';
+
+
+const typeDefs = readFileSync("graphql/schema.graphqls", 'utf-8');
+
+const loggingPlugin: ApolloServerPlugin = {
+    async requestDidStart() {
+        return {
+            async willSendResponse(requestContext) {
+                console.log("GraphQL Response:", requestContext.response)
+            },
+        };
+    },
+};
+
+const greetings = [
+    {
+        title: "Hello",
+        subtitle: "World!",
+    },
+    {
+        title: "Salutations",
+        subtitle: "Everyone!",
+    },
+];
+
+const resolvers = {
+    Query: {
+        greetings: () => greetings,
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [loggingPlugin]
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: {port: 4000, host: "127.0.0.1"},
+});
+
+console.log(`🚀 Server ready at ${url}`);
