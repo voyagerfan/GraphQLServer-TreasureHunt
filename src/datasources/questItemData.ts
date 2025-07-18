1c168d3e66ba8d8fa4e5ce2f67afcc0abd28270f
sv709 <sv709@yahoo.com>
2025-07-18 16:15:54 -0700

    add imageID and remove unused import

--- src/datasources/questItemData.ts
+++ src/datasources/questItemData.ts
@@ -2,8 +2,6 @@
 Data used for the questItems query for testing. Used in ./resolvers/questItemResolver.ts
 */
 
-import { Coordinate } from "../types/dataTypes.js";
-
 export const questItems = [
     {
         title: "Hello",
@@ -11,19 +9,7 @@ export const questItems = [
         clue: "test clue",
         hint: "test hint",
         coordinate: {latitude: 0.0, longitude: 0.0},
-        rating: 3.5
+        rating: 3.5,
+        endGameImageID: "testID.jpg"
     }
 ];
-
-
-/*
-data class QuestItem(
-    val title: String,
-    val description: String,
-    val clue: String,
-    val hint: String,
-    val coordinates: Coordinate,
-    val rating: Double,
-    val endGameAssets: CompletedQuestData
-)
-*/
