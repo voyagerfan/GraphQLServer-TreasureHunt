cfc7357e39005e5b08ded718b85e2d78ae6ee7a6
sv709 <sv709@yahoo.com>
2025-07-19 09:52:42 -0700

    add mathjs

--- src/utils/haversine.ts
+++ src/utils/haversine.ts
@@ -1,10 +1,12 @@
 import { Coordinate } from "../types/dataTypes.js"
 
+const earthRadius = 6371000
+
 export function Haversine(
     origin: Coordinate,
     destination: Coordinate
 ) : number {
-
+    
     
 
     return 0.0
