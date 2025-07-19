959c483707e1e6263f99f63a67b71ee638f14ce0
sv709 <sv709@yahoo.com>
2025-07-19 11:25:07 -0700

    remove mathjs, complete haversine implementation

--- src/utils/haversine.ts
+++ src/utils/haversine.ts
@@ -6,8 +6,22 @@ export function Haversine(
     origin: Coordinate,
     destination: Coordinate
 ) : number {
-    
-    
 
-    return 0.0
+    const deltaPhi = degreesToRadians(destination.latitude - origin.latitude);
+    const deltaLamba = degreesToRadians(destination.longitude - origin.longitude);
+
+    const angulardistance = 
+        Math.pow(Math.sin(deltaPhi/2), 2) + 
+        Math.cos(degreesToRadians(origin.latitude)) * Math.cos(degreesToRadians(destination.latitude)) *
+        Math.pow(Math.sin(deltaLamba/2), 2);
+    
+    const arcangle = 2 * Math.atan2(Math.sqrt(angulardistance), Math.sqrt(1-angulardistance));
+
+    return earthRadius * arcangle;
 }
+
+function degreesToRadians(degrees: number): number {
+    return degrees * Math.PI / 180;
+}
+
+    
