004a28399a9032e0025ba6e090adac8c86d2a197
sv709 <sv709@yahoo.com>
2025-07-20 11:17:06 -0700

    update schema

--- dist/resolvers/index.js
+++ dist/resolvers/index.js
@@ -6,9 +6,11 @@ This file combines all resolvers into in the ./resolvers into one export
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.resolvers = void 0;
 const greetingsResolver_js_1 = require("./greetingsResolver.js");
+const questItemResolver_js_1 = require("./questItemResolver.js");
 exports.resolvers = {
     Query: {
-        ...greetingsResolver_js_1.greetingsResolver.Query
+        ...greetingsResolver_js_1.greetingsResolver.Query,
+        ...questItemResolver_js_1.questItemResolver.Query
         // TODO: add more resolvers
     },
 };
